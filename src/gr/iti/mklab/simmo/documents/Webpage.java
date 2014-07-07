1f3efc7fdc26ad479b6cfd1aa11e0baaa77b62fa
amoumtzidou <moumtzid@iti.gr>
2014-07-07 18:14:30 +0300

    Add 'Webpage' class.

---
+++ src/gr/iti/mklab/simmo/documents/Webpage.java
@@ -0,0 +1,17 @@
+package gr.iti.mklab.simmo.documents;
+
+import gr.iti.mklab.simmo.Document;
+
+/**
+ * A Webpage is a {@link gr.iti.mklab.simmo.Document}. It may contain other {@link gr.iti.mklab.simmo.Item} objects.
+ * Webpages may contain Posts.
+ *
+ * @author amoumtzidou
+ * @version 1.0.0
+ * @see gr.iti.mklab.simmo.Document
+ * @since July 7, 2014
+ */
+public class Webpage extends Document {
+
+
+}
