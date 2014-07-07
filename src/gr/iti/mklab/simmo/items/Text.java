c5d5feae950806e77fada44197e4396157e046da
amoumtzidou <moumtzid@iti.gr>
2014-07-07 18:02:38 +0300

    Add 'Text' class. Check the enum (text type) variable

--- src/gr/iti/mklab/simmo/items/Text.java
+++ src/gr/iti/mklab/simmo/items/Text.java
@@ -12,4 +12,37 @@ import gr.iti.mklab.simmo.Item;
  * @author kandreadou
  */
 public class Text implements Item {
+	
+	/** Types of text */
+    private enum TEXT_TYPE{ASR,OCR,TXT,HTML};
+
+    
+    /**
+     * TODO: A path to the image thumbnail or a URL?
+     */
+    private TEXT_TYPE txtType;
+
+    
+    /** The content itself */
+    private String content;
+
+    
+    
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public TEXT_TYPE getTextType() {
+        return txtType;
+    }
+
+    public void setTextType(TEXT_TYPE txtType) {
+        this.txtType = txtType;
+    }
+    
+    
 }
