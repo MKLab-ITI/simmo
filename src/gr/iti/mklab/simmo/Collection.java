63519f0696e64bb093510733ee9b8fe63c6080df
amoumtzidou <moumtzid@iti.gr>
2014-07-10 17:38:15 +0300

    no message

--- src/gr/iti/mklab/simmo/Collection.java
+++ src/gr/iti/mklab/simmo/Collection.java
@@ -3,7 +3,7 @@ package gr.iti.mklab.simmo;
 /**
  * An Annotation interface which can be instantiated in many different ways.
  *
- * @author kandreadou
+ * @author amoumtzidou
  * @version 1.0.0
  * @since July 3, 2014
  */
