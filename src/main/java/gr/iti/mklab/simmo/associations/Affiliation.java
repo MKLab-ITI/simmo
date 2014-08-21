6c6c449678df9531bc9936affb9a4a770ce4000f
kandreadou <kandreadou@iti.gr>
2014-08-21 11:49:27 +0300

    Added fields to Affiliation

--- src/main/java/gr/iti/mklab/simmo/associations/Affiliation.java
+++ src/main/java/gr/iti/mklab/simmo/associations/Affiliation.java
@@ -2,6 +2,8 @@ package gr.iti.mklab.simmo.associations;
 
 import gr.iti.mklab.simmo.*;
 
+import java.util.Date;
+
 /**
  * Affiliation models the implicit relation between two {@link gr.iti.mklab.simmo.UserAccount}
  * e.g. be friends with, follows
@@ -12,14 +14,24 @@ import gr.iti.mklab.simmo.*;
  */
 public class Affiliation {
 
+    public enum AFFILIATION_TYPE {
+        FRIENDS_WITH, FOLLOWS, UNDEFINED
+    }
+
     protected UserAccount firstAccount;
 
     protected UserAccount secondAccount;
 
+    protected AFFILIATION_TYPE affiliationType;
+
+    protected Date affiliationDate;
+
     private String id;
 
-    public Affiliation (UserAccount account, UserAccount affiliated){
+    public Affiliation (UserAccount account, UserAccount affiliated, AFFILIATION_TYPE type, Date date){
         this.firstAccount = account;
         this.secondAccount = affiliated;
+        this.affiliationType = type;
+        this.affiliationDate = date;
     }
 }
