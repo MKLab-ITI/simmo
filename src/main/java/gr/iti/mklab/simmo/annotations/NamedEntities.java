29949c9191f6985479f78775f5405fb0765f8505
kandreadou <kandreadou@iti.gr>
2014-08-25 10:34:20 +0300

    Added NamedEntities

--- src/main/java/gr/iti/mklab/simmo/annotations/NamedEntities.java
+++ src/main/java/gr/iti/mklab/simmo/annotations/NamedEntities.java
@@ -2,5 +2,13 @@ package gr.iti.mklab.simmo.annotations;
 
 import gr.iti.mklab.simmo.associations.Annotation;
 
-public class NamedEntities implements Annotation {
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kandreadou
+ * @version 1.0.0
+ * @since August 25, 2014
+ */
+public class NamedEntities extends ArrayList<String> implements Annotation {
 }
