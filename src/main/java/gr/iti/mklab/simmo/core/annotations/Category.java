94908d8b4962afe5c1f7872cfc9cfa8df15ec077
amoumtzidou <moumtzid@iti.gr>
2015-10-08 10:23:59 +0300

    no message

--- src/main/java/gr/iti/mklab/simmo/core/annotations/Category.java
+++ src/main/java/gr/iti/mklab/simmo/core/annotations/Category.java
@@ -3,4 +3,19 @@ package gr.iti.mklab.simmo.core.annotations;
 import gr.iti.mklab.simmo.core.Annotation;
 
 public class Category implements Annotation {
+	
+	private String category;
+
+	public Category(){ }
+		
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+       
+    
 }
