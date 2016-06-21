05d9b47c184e4fa0f6ef0192f21fafdce9ad9eb3
manosetro <manosetro@iti.gr>
2016-06-21 18:57:48 +0300

    Add annotation to support distrurbing score on images/videos

---
+++ src/main/java/gr/iti/mklab/simmo/core/annotations/DisturbingScore.java
@@ -0,0 +1,21 @@
+package gr.iti.mklab.simmo.core.annotations;
+
+import gr.iti.mklab.simmo.core.Annotation;
+
+public class DisturbingScore implements Annotation {
+	
+	private double score;
+
+	public DisturbingScore(double score) {
+		this.score = score;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+}
