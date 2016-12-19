87bd7de5cd72feb026b0a2b0b7fe0b6b3c85ad16
Manos <Manos@192.168.1.2>
2016-12-19 16:35:41 +0200

    Add NsfwScore

---
+++ src/main/java/gr/iti/mklab/simmo/core/annotations/NsfwScore.java
@@ -0,0 +1,34 @@
+package gr.iti.mklab.simmo.core.annotations;
+
+import gr.iti.mklab.simmo.core.Annotation;
+
+public class NsfwScore implements Annotation {
+
+private double confidenceScore;
+	
+	private String annotationType = "DisturbingScore";
+	
+	public NsfwScore() {
+		
+	}
+	
+	public NsfwScore(double score) {
+		this.confidenceScore = score;
+	}
+	
+	public double getConfidenceScore() {
+		return confidenceScore;
+	}
+
+	public void setConfidenceScore(double score) {
+		this.confidenceScore = score;
+	}
+
+    public String getAnnotationType() {
+		return annotationType;
+	}
+
+	public void setAnnotationType(String annotationType) {
+		this.annotationType = annotationType;
+	}
+}
